c2ac1411d662f999e7654dd247840e6bc73439cc
Tharusha Kawshalya <tharusha.k.dev@gmail.com>
2025-08-14 03:12:34 +0530

    Day_3 Final

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import { connect } from 'mongoose';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import e from 'express';
 
 dotenv.config();
 
@@ -17,6 +19,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
